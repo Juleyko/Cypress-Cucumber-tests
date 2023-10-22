33e8b160abf735239e51d59c3dcba0f4e4e30dae
Julia Boreiko <juleyko7@gmail.com>
2023-10-22 21:34:01 +0300

    add reports

--- cypress.config.js
+++ cypress.config.js
@@ -30,14 +30,14 @@ const {
   addCucumberPreprocessorPlugin,
 } = require("@badeball/cypress-cucumber-preprocessor");
 const {
-  preprocessor,
+  preprocessor: browserify,
 } = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await addCucumberPreprocessorPlugin(on, config);
 
-  on("file:preprocessor", preprocessor(config));
+  on("file:preprocessor", browserify(config));
 
   // Make sure to return the config object as it might have been modified by the plugin.
   return config;
